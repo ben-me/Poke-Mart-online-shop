8f9b4fad4a186c6c3ada6a71c2528ab8a32ebf4b
Benedikt <>
2022-09-05 08:40:23 +0200

    style layout, add button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import ShoppingItem from './components/ShoppingItem';
+import styled from 'styled-components';
 
 function App() {
   const itemAPI = 'https://pokeapi.co/api/v2/item/';
@@ -13,13 +14,32 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Poké Mart Online Shop</h1>
-      <ul>
+      <Header>Poké Mart Online Shop</Header>
+      <UnorderedList>
         {shopInventory.map((item) => <ShoppingItem key={item.name} shopItem={item} />)}
       
-      </ul>
+      </UnorderedList>
     </div>
   );
 }
 
 export default App;
+
+const UnorderedList = styled.ul`
+  display:grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  list-style:none;
+  gap: 20px;
+  padding: 0;
+  margin: 0 auto;
+  justify-items: center;
+
+* {
+  box-sizing: border-box;
+}
+
+`
+
+const Header = styled.h1`
+  text-align: center;
+`
