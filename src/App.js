85cacedfff6807c6a902a6d40d7eafd619e06bd6
Ben Me <benedikt.mehler@mailbox.org>
2022-09-05 10:26:37 +0200

    seperate pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import ShoppingItem from "./components/ShoppingItem";
 import styled from "styled-components";
-import CartItem from "./components/CartItem";
 import { Link, Route, Routes } from "react-router-dom";
-import cart from "./assets/cart.png";
+import Home from "./Pages/Home";
+
+import ShoppingCart from "./Pages/ShoppingCart";
 
 function App() {
   const itemAPI = "https://pokeapi.co/api/v2/item/";
@@ -43,40 +43,20 @@ function App() {
         <Route
           path="/shopping-cart"
           element={
-            <>
-              <h2>Cart Items</h2>
-              <UnorderedList>
-                {shoppingCart.map((item) => (
-                  <CartItem
-                    key={item.name}
-                    itemInCart={item}
-                    onRemoveFromCart={removeFromCart}
-                  />
-                ))}
-              </UnorderedList>
-              <BackToShop to="/"> Back to the Shop</BackToShop>
-            </>
+            <ShoppingCart
+              shoppingCart={shoppingCart}
+              removeFromCart={removeFromCart}
+            />
           }
         />
         <Route
           path="/"
           element={
-            <>
-              <StyledLink to="/shopping-cart">
-                <CartImage src={cart} alt="" />
-                <AmountOfItems>{shoppingCart.length}</AmountOfItems>
-              </StyledLink>
-              <h2>Shop Items</h2>
-              <UnorderedList>
-                {shopInventory.map((item) => (
-                  <ShoppingItem
-                    key={item.name}
-                    shopItem={item}
-                    onAddToCart={addToCart}
-                  />
-                ))}
-              </UnorderedList>
-            </>
+            <Home
+              shoppingCart={shoppingCart}
+              shopInventory={shopInventory}
+              addToCart={addToCart}
+            />
           }
         />
       </Routes>
@@ -86,58 +66,6 @@ function App() {
 
 export default App;
 
-const UnorderedList = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  list-style: none;
-  gap: 20px;
-  padding: 0;
-  margin: 0 auto;
-  justify-items: center;
-
-  * {
-    box-sizing: border-box;
-  }
-`;
-
 const Header = styled.h1`
   text-align: center;
-`;
-
-const CartImage = styled.img`
-   {
-    display: block;
-    width: 150px;
-    max-width: 100%;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  display: block;
-  margin-right: 0;
-  margin-left: auto;
-  width: 200px;
-  text-decoration: none;
-  position: relative;
-`;
-
-const BackToShop = styled(Link)`
-  color: black;
-  font-size: xx-large;
-  text-align: center;
-  width: 100%;
-  display: inline-block;
-  margin-top: 5rem;
-`;
-
-const AmountOfItems = styled.p`
-  border: solid black 1px;
-  background-color: red;
-  border-radius: 50%;
-  width: 20px;
-  color: black;
-  text-align: center;
-  position: absolute;
-  right: 10rem;
-  bottom: -5px;
 `;
