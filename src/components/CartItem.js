46fc1f1df373f06dab86637bd3d546459319134c
Ben Me <benedikt.mehler@mailbox.org>
2022-09-05 10:36:20 +0200

    fix small unused stuff

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,38 +1,43 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-export default function CartItem({itemInCart, onRemoveFromCart}){
-    
-    
-    function handleClick(){
-        onRemoveFromCart(itemInCart);
-    }
+export default function CartItem({ itemInCart, onRemoveFromCart }) {
+  function handleClick() {
+    onRemoveFromCart(itemInCart);
+  }
 
-    return <Listelement><img src={itemInCart.image}/>{itemInCart.name} <button type='button' onClick={handleClick}>Remove Item</button></Listelement>
+  return (
+    <Listelement>
+      <img src={itemInCart.image} alt="" />
+      {itemInCart.name}{" "}
+      <button type="button" onClick={handleClick}>
+        Remove Item
+      </button>
+    </Listelement>
+  );
 }
 
 const Listelement = styled.li`
-{
+   {
     padding: 20px;
     border: 1px black solid;
-    background-color:white;
+    background-color: white;
     border-radius: 15px;
     width: 250px;
     height: 120px;
     padding: 5px;
-    display:grid;
+    display: grid;
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 1fr 1fr;
-    justify-items:center;
-    align-items:center;
-
+    justify-items: center;
+    align-items: center;
 
     img {
-        grid-row: 1 / 4;
-        width: 60%;
-        object-fit:contain;
+      grid-row: 1 / 4;
+      width: 60%;
+      object-fit: contain;
     }
-    p{
-        margin:0;
+    p {
+      margin: 0;
     }
-}
-`
+  }
+`;
--- src/components/ShoppingItem.js
+++ src/components/ShoppingItem.js
@@ -1,59 +1,65 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-export default function ShoppingItem({shopItem, onAddToCart}){
-    const [itemDetail, setItemDetail] = useState({})
-    const detailURL = shopItem.url;
+export default function ShoppingItem({ shopItem, onAddToCart }) {
+  const [itemDetail, setItemDetail] = useState({});
+  const detailURL = shopItem.url;
 
-    useEffect(() => {
-        fetch(detailURL).then((response) => response.json())
-        .then((data) => {
-            const newItem = {
-                name: data.name,
-                image: data.sprites.default,
-                cost: data.cost
-            }
-            setItemDetail(newItem);
-        });
-    }, [detailURL])
+  useEffect(() => {
+    fetch(detailURL)
+      .then((response) => response.json())
+      .then((data) => {
+        const newItem = {
+          name: data.name,
+          image: data.sprites.default,
+          cost: data.cost,
+        };
+        setItemDetail(newItem);
+      });
+  }, [detailURL]);
 
-    function handleClick() {
-        onAddToCart(itemDetail);
-    }
+  function handleClick() {
+    onAddToCart(itemDetail);
+  }
 
-
-    return <Listelement><img src={itemDetail.image} alt=""/>{shopItem.name} <p>{itemDetail.cost}</p> <button type="button" onClick={handleClick} >Add Item</button></Listelement>
+  return (
+    <Listelement>
+      <img src={itemDetail.image} alt="" />
+      {shopItem.name} <p>{itemDetail.cost}</p>{" "}
+      <Button type="button" onClick={handleClick}>
+        Add Item
+      </Button>
+    </Listelement>
+  );
 }
 
 const Listelement = styled.li`
-{
+   {
     padding: 20px;
     border: 1px black solid;
-    background-color:white;
+    background-color: white;
     border-radius: 15px;
     width: 250px;
     height: 120px;
     padding: 5px;
-    display:grid;
+    display: grid;
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 1fr 1fr 1fr;
-    justify-items:center;
-    align-items:center;
-
+    justify-items: center;
+    align-items: center;
 
     img {
-        grid-row: 1 / 4;
-        width: 60%;
-        object-fit:contain;
+      grid-row: 1 / 4;
+      width: 60%;
+      object-fit: contain;
     }
-    p{
-        margin:0;
+    p {
+      margin: 0;
     }
-}
-`
-
+  }
+`;
 const Button = styled.button`
-    background:none;
-    border:1px solid black;
-    border-radius: 5px;
-`
+  background: azure;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
